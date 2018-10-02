4e81b65b6d9dfe8a3e885b1b083cba5b676dcbaa
Anders Olav Candasamy <anders.can1@gmail.com>
2018-10-02 12:04:52 +0200

    add error spec

---
+++ test/TypedWorker.error.spec.ts
@@ -0,0 +1,65 @@
+import { createWorker, ITypedWorker } from '../src/index'
+
+describe('try/catch handling', function() {
+  describe('try/catch - success', () => {
+    let result: string
+
+    beforeEach(function(done) {
+      const tryWorker: ITypedWorker<string, string> = createWorker(
+        (input, cb) => {
+          try {
+            if (input === 'works') cb('ok')
+            throw 'not ok'
+          } catch (e) {
+            cb(e)
+          }
+          return input
+        },
+        output => {
+          result = output
+          done()
+        }
+      )
+      tryWorker.postMessage('works')
+    })
+
+    it('returns the correct type', function() {
+      expect(result).toEqual(jasmine.any(String))
+    })
+
+    it('returns the correct value', function() {
+      expect(result).toEqual('ok')
+    })
+  })
+
+  describe('try/catch - failure', () => {
+    let result: string
+
+    beforeEach(function(done) {
+      const tryWorker: ITypedWorker<string, string> = createWorker(
+        (input, cb) => {
+          try {
+            if (input === 'works') cb('ok')
+            throw 'not ok'
+          } catch (e) {
+            cb(e)
+          }
+          return input
+        },
+        output => {
+          result = output
+          done()
+        }
+      )
+      tryWorker.postMessage('does not work')
+    })
+
+    it('returns the correct type', function() {
+      expect(result).toEqual(jasmine.any(String))
+    })
+
+    it('returns the correct value', function() {
+      expect(result).toEqual('not ok')
+    })
+  })
+})
